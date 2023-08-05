d0ce4bf748742f9dfdb98dc7bfe71694d1b076ad
Dede ariansya <dedeariansya1@gmail.com>
2023-08-05 21:26:33 +0800

    🖥️ wip: Setup redis, minio, and logging.

---
+++ src/config/config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: path.join(__dirname, '../../.env') })
+
+const config = {
+  isProd: process.env.NODE_ENV === 'production',
+  isDev: process.env.NODE_ENV !== 'production',
+
+  logging: process.env.LOGGING === 'true',
+
+  redis: {
+    socket: {
+      host: String(process.env.REDIS_HOST || 'localhost'),
+      port: Number(process.env.REDIS_PORT || 6379),
+    },
+    username: String(process.env.REDIS_USER || 'default'),
+    password: String(process.env.REDIS_PASS || 'null'),
+  },
+
+  minio: {
+    bucket: String(process.env.MINIO_BUCKET || 'local'),
+    region: String(process.env.MINIO_REGION || 'us-east-1'),
+    config: {
+      endPoint: String(process.env.MINIO_ENDPOINT || 'localhost'),
+      port: Number(process.env.MINIO_PORT || 9000),
+      useSSL: process.env.MINIO_USE_SSL === 'true',
+      accessKey: String(process.env.MINIO_ACCESS_KEY || 'sail'),
+      secretKey: String(process.env.MINIO_SECRET_KEY || 'password'),
+    },
+  },
+}
+
+module.exports = config
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('Hello World!')
+const config = require('./config/config')
+const Minio = require('minio')
+const redis = require('redis')
+const logger = require('./config/logger')
+
+const minioClient = new Minio.Client(config.minio.config)
+const bucketName = config.minio.bucket
+
+const redisClient = redis.createClient(config.redis)
+
+redisClient.on('error', (err) => logger.error('Redis Client Error', err))
+;(async () => {
+  const bucketsList = await minioClient.listBuckets()
+  logger.info(`Buckets List: ${bucketsList.map((bucket) => bucket.name).join(',\t')}`)
+
+  await redisClient.connect()
+
+  await redisClient.set('key', 'value')
+
+  const val = await redisClient.get('key')
+  logger.info(val)
+
+  await redisClient.disconnect()
+})()
